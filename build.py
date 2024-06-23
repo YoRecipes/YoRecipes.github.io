4e27b5c43b59379bf466c750e37d7576bd82c25b
pauldubois98 <pauldubois98@gmail.com>
2024-06-23 22:28:53 +0200

    search for pdf instead of docx

--- build.py
+++ build.py
@@ -16,7 +16,7 @@ file_build.write(top_html + '\n')
 lines = []
 for folder in glob.glob('*/'):
     folder_class = folder.replace('_', '-').replace('\\', '')
-    for file in glob.glob(folder+'*.docx'):
+    for file in glob.glob(folder+'*.pdf'):
         name = file.split('\\')[1].split('.')[0]
         if name[0] == '_':
             continue
