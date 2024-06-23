7277df1b5725d8626715633509c09219fc71531e
pauldubois98 <pauldubois98@gmail.com>
2024-06-23 20:42:28 +0200

    sort recipies by name

--- build.py
+++ build.py
@@ -11,13 +11,19 @@ file_bottom.close()
 
 file_build = open('index.html', 'w')
 file_build.write(top_html)
+
+lines = []
 for folder in glob.glob('*/'):
     folder_class = folder.replace('_', '-')
     for file in glob.glob(folder+'*.docx'):
         name = file.split('\\')[1].split('.')[0]
         if name[0] == '_':
             continue
-        file_build.write(f'        <li class="recipe {folder_class}"><a>{name}</a></li>\n')
-        # print(folder, name)
+        lines.append( (name, f'        <li class="recipe {folder_class}"><a>{name}</a></li>\n') )
+
+lines.sort()
+for line in lines:
+    file_build.write(line[1])
+
 file_build.write(bottom_html)
 file_build.close()
