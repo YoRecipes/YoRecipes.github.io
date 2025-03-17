ff01b30782a239aa224e1cbebe7ce69f006606b2
pauldubois98 <pauldubois98@gmail.com>
2025-03-18 00:36:02 +0100

    import libs

---
+++ build.py
@@ -0,0 +1,18 @@
+# Using the library "docx2pdf" (https://github.com/AlJohri/docx2pdf)
+# `pip install docx2pdf`
+from docx2pdf import convert
+# Using the library "pdf2image" (https://pypi.org/project/pdf2image/)
+# `pip install pdf2image`
+from pdf2image import convert_from_path
+# Using the library "deep_translator" (https://pypi.org/project/deep-translator/)
+# `pip install deep-translator`
+from deep_translator import GoogleTranslator
+# Using the library "docx" (https://python-docx.readthedocs.io/en/latest/)
+# `pip install python-docx`
+import docx
+# Using the library "unidecode" (https://pypi.org/project/Unidecode/)
+# `pip install Unidecode`
+from unidecode import unidecode
+# standard libraries
+import json
+import os
