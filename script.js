651223ee6c5c17d7c3059e0404f4dd168ac356a0
pauldubois98 <pauldubois98@gmail.com>
2025-03-19 16:39:08 +0100

    dark mode script

---
+++ script.js
@@ -0,0 +1,33 @@
+const darkThemeButton = document.getElementById("dark-theme");
+const lightThemeButton = document.getElementById("light-theme");
+
+function lightTheme() {
+  document.body.classList.remove("dark-mode");
+  darkThemeButton.style.display = "inline-block";
+  lightThemeButton.style.display = "none";
+  localStorage.setItem("theme", "light");
+}
+function darkTheme() {
+  document.body.classList.add("dark-mode");
+  darkThemeButton.style.display = "none";
+  lightThemeButton.style.display = "inline-block";
+  localStorage.setItem("theme", "dark");
+}
+darkThemeButton.addEventListener("click", darkTheme);
+lightThemeButton.addEventListener("click", lightTheme);
+
+// get theme from local storage
+const localTheme = localStorage.getItem("theme");
+if (localTheme === "dark") {
+  darkTheme();
+} else {
+  lightTheme();
+}
+// get parameter from URL
+const urlParams = new URLSearchParams(window.location.search);
+const theme = urlParams.get("theme");
+if (theme === "dark") {
+  darkTheme();
+} else if (theme === "light") {
+  lightTheme();
+}
